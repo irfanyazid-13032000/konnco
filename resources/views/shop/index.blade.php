03190e03437afb59df5f3657ecc3594c8c260051
irfanyazid-13032000 <irfanyazid18@gmail.com>
2024-12-11 19:48:07 +0700

    design index sudah lumayan bagus berkat chatGPT

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,41 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Yazid Toko Online</title>
-    <link rel="stylesheet" href="{{ asset('css/shop/index.css') }}">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Modern Navbar and Shop</title>
+  <link rel="stylesheet" href="{{ asset('css/shop/index.css') }}">
+  
 </head>
 <body>
+  <nav class="navbar">
+    <div class="logo">Yazid Store</div>
+    <ul class="menu">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+    <a href="#" class="cta">Cart (13)</a>
+  </nav>
 
-<div class="shop">
-  <div class="container">
+  <div class="shop">
+    <div class="container">
+      <div class="card">
+        <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2024/11/4/2d46ac2e-8ddf-4cd2-8a8e-c6396ca60a81.png" alt="">
+        <p class="judul">Sabun Nuvo Nih</p>
+        <p class="harga">Rp.23.000</p>
+      </div>
 
-
-    <div class="card">
-      <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2024/11/4/2d46ac2e-8ddf-4cd2-8a8e-c6396ca60a81.png" alt="">
-      <p class="judul">sabun nuvo nih</p>
-      <p class="harga">Rp.23.000</p>
-      <button class="beli">Beli</button>
+      <div class="card">
+        <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2024/11/4/2d46ac2e-8ddf-4cd2-8a8e-c6396ca60a81.png" alt="">
+        <p class="judul">Sabun Nuvo Nih</p>
+        <p class="harga">Rp.23.000</p>
+      </div>
+      <div class="card">
+        <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2024/11/4/2d46ac2e-8ddf-4cd2-8a8e-c6396ca60a81.png" alt="">
+        <p class="judul">Sabun Nuvo Nih</p>
+        <p class="harga">Rp.23.000</p>
+      </div>
     </div>
-
-
-    <div class="card">
-      <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2024/11/4/2d46ac2e-8ddf-4cd2-8a8e-c6396ca60a81.png" alt="">
-      <p>sabun nuvo nih</p>
-      <p class="harga">Rp.23.000</p>
-      <button class="beli">Beli</button>
-    </div>
-  
-
-
-
-
-
-
   </div>
-</div>
-
-
 </body>
 </html>
