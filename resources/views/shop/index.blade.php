3a96d13a4c2b239724905c9665df04f8f2a2a5ea
irfanyazid-13032000 <irfanyazid18@gmail.com>
2024-12-11 14:41:55 +0700

    membuat tampilan untuk user

---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yazid Toko Online</title>
+    <link rel="stylesheet" href="{{ asset('css/shop/index.css') }}">
+</head>
+<body>
+
+<div class="shop">
+  <div class="container">
+
+
+    <div class="card">
+      <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2024/11/4/2d46ac2e-8ddf-4cd2-8a8e-c6396ca60a81.png" alt="">
+      <p class="judul">sabun nuvo nih</p>
+      <p class="harga">Rp.23.000</p>
+      <button class="beli">Beli</button>
+    </div>
+
+
+    <div class="card">
+      <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2024/11/4/2d46ac2e-8ddf-4cd2-8a8e-c6396ca60a81.png" alt="">
+      <p>sabun nuvo nih</p>
+      <p class="harga">Rp.23.000</p>
+      <button class="beli">Beli</button>
+    </div>
+  
+
+
+
+
+
+
+  </div>
+</div>
+
+
+</body>
+</html>
