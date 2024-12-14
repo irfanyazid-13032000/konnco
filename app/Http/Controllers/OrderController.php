789e01bf34581bf080041d3a9452ed6e79dd9fc2
irfanyazid-13032000 <irfanyazid18@gmail.com>
2024-12-14 16:06:36 +0700

    berhasil membuat insert ke order detail

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\Item;
 use Illuminate\Support\Str;
 
 
@@ -31,15 +33,28 @@ class OrderController extends Controller
     public function store(Request $request)
     {
 
-        return response()->json($request);
-
-        
-
+        // return response()->json($request);
+        $total_order = 0;
         $receipt_number = Str::random(40);
+        
+        foreach ($request->checkout_products as $checkout_product) {
+            
+            $product_data = Item::find($checkout_product['product_id']);
+            $total_order += $product_data->price * $checkout_product['qty'];
+
+            OrderDetail::create([
+                'receipt_number'          => $receipt_number,
+                'qty'                     => $checkout_product['qty'],
+                'item_id'                 => $checkout_product['product_id'],
+                'price'                   => $product_data->price,
+                'total_price_per_item' => $product_data->price * $checkout_product['qty']
+            ]);
+            
+        }
 
         Order::create([
-            // 'total_order' => $validatedData['total_order'],
-            'customer_id' => session('customer_login_id'),
+            'total_order'    => $total_order,
+            'customer_id'    => session('customer_login_id'),
             'receipt_number' => $receipt_number,
         ]);
 
